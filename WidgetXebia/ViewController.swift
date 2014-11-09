25d608fdbbe3d64305d4ee86fb31d1032fb98ca1
Deepak Kumar <DeepakKumar@Deepaks-MacBook-Pro.local>
2014-11-09 11:25:00 +0530

    Initial Commit

---
+++ WidgetXebia/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WidgetXebia
+//
+//  Created by Deepak Kumar on 09/11/14.
+//  Copyright (c) 2014 Deepak Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
